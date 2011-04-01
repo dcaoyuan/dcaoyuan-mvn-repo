a62ade00c4d779faf51e542f07df306efe7afd20
Caoyuan Deng <dcaoyuan@gmail.com>
2011-04-02 02:41:45 +0800

    Added org.eclipse.jgit-parent

--- deploy-jgit.sh
+++ deploy-jgit.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-cd ../../jgit/org.eclipse.jgit
-mvn -DaltDeploymentRepository=snapshots::default::file:../../dcaoyuan/dcaoyuan-mvn-repo/snapshots clean deploy
+cd ../../jgit
+mvn -Dmaven.test.skip=true -DaltDeploymentRepository=snapshots::default::file:../dcaoyuan/dcaoyuan-mvn-repo/snapshots deploy
+cd ../dcaoyuan/dcaoyuan-mvn-repo/snapshots/org/eclipse/jgit
+rm -rf org.eclipse.jgit.console
+rm -rf org.eclipse.jgit.http.server
+rm -rf org.eclipse.jgit.http.test
+rm -rf org.eclipse.jgit.iplog
+rm -rf org.eclipse.jgit.junit
+rm -rf org.eclipse.jgit.junit.http
+rm -rf org.eclipse.jgit.pgm
+rm -rf org.eclipse.jgit.test
+rm -rf org.eclipse.jgit.ui
+
